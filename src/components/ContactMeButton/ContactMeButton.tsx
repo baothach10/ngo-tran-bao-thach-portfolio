b8136764561854c5392beea873104f3684cb27b5
baothach10 <baothach10@gmail.com>
2025-08-11 18:43:47 +0700

    feat: adjust contact me direction to the contact me page

--- src/components/ContactMeButton/ContactMeButton.tsx
+++ src/components/ContactMeButton/ContactMeButton.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './ContactMeButton.css';
 
 type TContactMeButton = {
@@ -8,10 +9,10 @@ const ContactMeButton = ({ content = 'Contact Me' }: TContactMeButton) => {
     return (
         <div id="contact-me-wrapper">
 
-            <a className="contact-me-btn" target='_blank' href="https://www.upwork.com/freelancers/~01f0eb39f3a608ad31?viewMode=1">
+            <Link className="contact-me-btn" to={'/contact-me'} aria-label="Contact Me">
                 <span className="contact-me-text">{content}</span>
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.66669 11.3334L11.3334 4.66669" stroke="white" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round" /><path d="M4.66669 4.66669H11.3334V11.3334" stroke="white" strokeWidth="1.33333" strokeLinecap="round" /></svg>
-            </a>
+            </Link>
 
         </div>
     );
